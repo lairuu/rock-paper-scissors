7318903e0e6740365d669e577035f1e1b94c7096
lairuu <151829893+lairuu@users.noreply.github.com>
2023-12-19 16:04:30 +0100

    Improved script with prompt for move, etc...

--- script.js
+++ script.js
@@ -1,19 +1,110 @@
+
+let playerSelection = prompt(`What's your move ?`)
+
+
 function getComputerChoice() {
     let randomNumber = (Math.random())
 
     let computerChoice = ''
 
     if (randomNumber < 1/3) {
-        computerChoice = 'Rock'
+        computerChoice = 'rock'
     }
     else if (randomNumber > 1/3 && randomNumber < 2/3) {
-        computerChoice = 'Paper'
+        computerChoice = 'paper'
     }
     else if (randomNumber > 2/3) {
-        computerChoice = 'Scissors'
+        computerChoice = 'scissors'
     }
 
-    console.log(computerChoice)
+    return computerChoice;
 }
 
-getComputerChoice()
+function playRound(playerSelection, computerSelection) {
+    if ( playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper') {
+        return "You Lose! Paper beats Rock";
+    }
+    else if ( playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') {
+        return "You Win! Rock beats Scissors";
+    }
+    else if ( playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'rock') {
+        return `It's a Tie!`
+    } 
+}
+
+
+
+function game() {
+    let playerScore = Number('0')
+    let computerScore = Number('0')
+
+    if (playRound(playerSelection, getComputerChoice()) === "You Lose! Paper beats Rock") {
+        computerScore++
+        console.log(`Computer's win`)
+    } 
+    else if (playRound(playerSelection, getComputerChoice()) === "You Win! Rock beats Scissors") {
+        playerScore++
+        console.log(`Player's win`)
+    }
+    else if (playRound(playerSelection, getComputerChoice()) === "It's a Tie!") {
+        console.log('Tie')
+    }
+    if (playRound(playerSelection, getComputerChoice()) === "You Lose! Paper beats Rock") {
+        computerScore++
+        console.log(`Computer's win`)
+    } 
+    else if (playRound(playerSelection, getComputerChoice()) === "You Win! Rock beats Scissors") {
+        playerScore++
+        console.log(`Player's win`)
+    }
+    else if (playRound(playerSelection, getComputerChoice()) === "It's a Tie!") {
+        console.log('Tie')
+    }
+    if (playRound(playerSelection, getComputerChoice()) === "You Lose! Paper beats Rock") {
+        computerScore++
+        console.log(`Computer's win`)
+    } 
+    else if (playRound(playerSelection, getComputerChoice()) === "You Win! Rock beats Scissors") {
+        playerScore++
+        console.log(`Player's win`)
+    }
+    else if (playRound(playerSelection, getComputerChoice()) === "It's a Tie!") {
+        console.log('Tie')
+    }
+    if (playRound(playerSelection, getComputerChoice()) === "You Lose! Paper beats Rock") {
+        computerScore++
+        console.log(`Computer's win`)
+    } 
+    else if (playRound(playerSelection, getComputerChoice()) === "You Win! Rock beats Scissors") {
+        playerScore++
+        console.log(`Player's win`)
+    }
+    else if (playRound(playerSelection, getComputerChoice()) === "It's a Tie!") {
+        console.log('Tie')
+    }
+    if (playRound(playerSelection, getComputerChoice()) === "You Lose! Paper beats Rock") {
+        computerScore++
+        console.log(`Computer's win`)
+    } 
+    else if (playRound(playerSelection, getComputerChoice()) === "You Win! Rock beats Scissors") {
+        playerScore++
+        console.log(`Player's win`)
+    }
+    else if (playRound(playerSelection, getComputerChoice()) === "It's a Tie!") {
+        console.log('Tie')
+    }
+    
+    
+
+    if (playerScore > computerScore) {
+        return 'You win !'
+    }
+    else if (computerScore > playerScore) {
+        return 'You lose !'
+    }
+    else {
+        return `It's a Tie!`
+    }
+}
+
+console.log(game())
