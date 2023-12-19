fbc4fbad9b4ba73c64713d0774198d7e4525aa45
lairuu <151829893+lairuu@users.noreply.github.com>
2023-12-19 17:00:59 +0100

    Reduced code with helper functions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 let playerSelection = prompt(`What's your move ?`)
-
+let computerSelection = ''
 
 function getComputerChoice() {
     let randomNumber = (Math.random())
@@ -17,94 +17,107 @@ function getComputerChoice() {
         computerChoice = 'scissors'
     }
 
-    return computerChoice;
+    computerSelection = computerChoice;
+    return computerSelection
 }
+
+
+
 
 function playRound(playerSelection, computerSelection) {
-    if ( playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper') {
-        return "You Lose! Paper beats Rock";
+    
+    getComputerChoice()
+
+    if (playerSelection.toLowerCase() === 'rock') {
+       return playerRock(playerSelection, computerSelection);
+
     }
-    else if ( playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') {
-        return "You Win! Rock beats Scissors";
+    else if (playerSelection.toLowerCase() === 'scissors') {
+        return playerScissors(playerSelection, computerSelection);
+
     }
-    else if ( playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'rock') {
-        return `It's a Tie!`
-    } 
+    else if (playerSelection.toLowerCase() === 'paper') {
+        return playerPaper(playerSelection, computerSelection);
+
+    }
+    else {
+        alert('This is not a valid choice')
+    }
+    
+     
 }
 
+
+
+function playerRock(playerSelection, computerSelection) {
+    if ( playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper') {
+        return "Loss";
+    }
+    else if ( playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') {
+        return "Win";
+    }
+    else if ( playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'rock') {
+        return `Tie`;
+    }
+}
+
+function playerScissors(playerSelection, computerSelection) {
+    if ( playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'paper') {
+        return "Win";
+    }
+    else if ( playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'scissors') {
+        return `Tie`;
+    }
+    else if ( playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'rock') {
+        return 'Loss';
+    }
+}
+
+function playerPaper(playerSelection, computerSelection) {
+    if ( playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'paper') {
+        return `Tie`;
+    }
+    else if ( playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'scissors') {
+        return 'Loss';
+    }
+    else if ( playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') {
+        return 'Win';
+    }
+}
 
 
 function game() {
-    let playerScore = Number('0')
-    let computerScore = Number('0')
-
-    if (playRound(playerSelection, getComputerChoice()) === "You Lose! Paper beats Rock") {
-        computerScore++
-        console.log(`Computer's win`)
-    } 
-    else if (playRound(playerSelection, getComputerChoice()) === "You Win! Rock beats Scissors") {
-        playerScore++
-        console.log(`Player's win`)
-    }
-    else if (playRound(playerSelection, getComputerChoice()) === "It's a Tie!") {
-        console.log('Tie')
-    }
-    if (playRound(playerSelection, getComputerChoice()) === "You Lose! Paper beats Rock") {
-        computerScore++
-        console.log(`Computer's win`)
-    } 
-    else if (playRound(playerSelection, getComputerChoice()) === "You Win! Rock beats Scissors") {
-        playerScore++
-        console.log(`Player's win`)
-    }
-    else if (playRound(playerSelection, getComputerChoice()) === "It's a Tie!") {
-        console.log('Tie')
-    }
-    if (playRound(playerSelection, getComputerChoice()) === "You Lose! Paper beats Rock") {
-        computerScore++
-        console.log(`Computer's win`)
-    } 
-    else if (playRound(playerSelection, getComputerChoice()) === "You Win! Rock beats Scissors") {
-        playerScore++
-        console.log(`Player's win`)
-    }
-    else if (playRound(playerSelection, getComputerChoice()) === "It's a Tie!") {
-        console.log('Tie')
-    }
-    if (playRound(playerSelection, getComputerChoice()) === "You Lose! Paper beats Rock") {
-        computerScore++
-        console.log(`Computer's win`)
-    } 
-    else if (playRound(playerSelection, getComputerChoice()) === "You Win! Rock beats Scissors") {
-        playerScore++
-        console.log(`Player's win`)
-    }
-    else if (playRound(playerSelection, getComputerChoice()) === "It's a Tie!") {
-        console.log('Tie')
-    }
-    if (playRound(playerSelection, getComputerChoice()) === "You Lose! Paper beats Rock") {
-        computerScore++
-        console.log(`Computer's win`)
-    } 
-    else if (playRound(playerSelection, getComputerChoice()) === "You Win! Rock beats Scissors") {
-        playerScore++
-        console.log(`Player's win`)
-    }
-    else if (playRound(playerSelection, getComputerChoice()) === "It's a Tie!") {
-        console.log('Tie')
-    }
     
-    
+    let playerScore = 0
+    let computerScore = 0
 
+
+    for (let i = 0; i < 5; i++) {
+        if (playRound(playerSelection, computerSelection) === "Win") {
+            playerScore++
+            console.log(`Player's win`)
+        } 
+        else if (playRound(playerSelection, computerSelection) === "Loss") {
+            computerScore++
+            console.log(`Computer's win`)
+        }
+        else if (playRound(playerSelection, computerSelection) === "Tie") {
+            console.log('Tie')
+        }
+
+    console.log(playerScore, computerScore)
+}
+    
     if (playerScore > computerScore) {
         return 'You win !'
     }
     else if (computerScore > playerScore) {
         return 'You lose !'
     }
-    else {
+    else if (computerScore === playerScore) {
         return `It's a Tie!`
     }
 }
 
 console.log(game())
+
